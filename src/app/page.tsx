e4bc620289bbe62d10eabd23b736d6f2e8313b6f
blac-coffee <soumilgupta.812@gmail.com>
2026-03-17 22:52:01 +0530

    07:ai providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,13 @@ export default function Home() {
   const trpc = useTRPC();
   const queryClient = useQueryClient();
   const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const testAi = useMutation(
+    trpc.testAi.mutationOptions({
+      onSuccess: () => {
+        toast.success("Ai request Queued");
+      },
+    }),
+  );
   const create = useMutation(
     trpc.createWorkflow.mutationOptions({
       onSuccess: () => {
@@ -19,6 +26,9 @@ export default function Home() {
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       protected route{JSON.stringify(data, null, 2)}
+      <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>
+        Test AI
+      </Button>
       <Button disabled={create.isPending} onClick={() => create.mutate()}>
         Create Workflow
       </Button>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,48 @@
 import { inngest } from "./client";
 import prisma from "@/lib/db";
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world", triggers: [{ event: "test/hello.world" }] },
+import { google } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import { createAnthropic } from "@ai-sdk/anthropic";
+import { generateText } from "ai";
+
+const openai = createOpenAI({});
+const anthropic = createAnthropic({});
+export const executeAi = inngest.createFunction(
+  { id: "execute-ai", triggers: [{ event: "execute/ai" }] },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    await step.run("create-workflow", () => {
-      return prisma.workflow.create({
-        data: {
-          name: "test-workflow-from-inngest",
-        },
-      });
-    });
+    await step.sleep("pretend-work", "5s");
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-generate-text",
+      generateText,
+      {
+        model: google("gemini-3.1-flash-lite-preview"),
+        system: "You are a helpful assistant.",
+        prompt: "what is 2+4?",
+      },
+    );
+    const { steps: openaiSteps } = await step.ai.wrap(
+      "openai-generate-text",
+      generateText,
+      {
+        model: openai("gpt-4.1"),
+        system: "You are a helpful assistant.",
+        prompt: "what is 2+4?",
+      },
+    );
+    const { steps: anthropicSteps } = await step.ai.wrap(
+      "anthropic-generate-text",
+      generateText,
+      {
+        model: anthropic("claude-sonnet-4-0"),
+        system: "You are a helpful assistant.",
+        prompt: "what is 2+4?",
+      },
+    );
+
+    return {
+      geminiSteps,
+      openaiSteps,
+      anthropicSteps,
+    };
   },
 );
